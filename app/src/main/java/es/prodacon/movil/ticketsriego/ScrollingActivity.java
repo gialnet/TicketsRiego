091e191948d83b8c02bd34899d71b445fa209144
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2016-05-16 14:29:44 +0200

    Lectura de código QR

--- app/src/main/java/es/prodacon/movil/ticketsriego/ScrollingActivity.java
+++ app/src/main/java/es/prodacon/movil/ticketsriego/ScrollingActivity.java
@@ -1,25 +1,29 @@
 package es.prodacon.movil.ticketsriego;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-import static es.prodacon.movil.ticketsriego.R.xml.preferencias;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.text.BreakIterator;
 
 public class ScrollingActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,7 @@ public class ScrollingActivity extends AppCompatActivity {
 
     /**
      * Arrancar el motor
+     *
      * @param view
      */
     public void execArrancaMotor(View view) {
@@ -55,13 +60,14 @@ public class ScrollingActivity extends AppCompatActivity {
 
                 return;
             }
-            SharedPreferences prefs = getSharedPreferences("preferencias",MODE_PRIVATE);
-            String tlf_motor = prefs.getString("tlf_motor","Ajuste valores");
+            SharedPreferences prefs = getSharedPreferences("preferencias", MODE_PRIVATE);
+            String tlf_motor = prefs.getString("tlf_motor", "Ajuste valores");
             startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + tlf_motor)));
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -86,26 +92,48 @@ public class ScrollingActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    /**
+     * @param view
+     */
+    public void scanQRCode(View view) {
+        //IntentIntegrator integrator = new IntentIntegrator(this);
+        //integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES);
+        new IntentIntegrator(ScrollingActivity.this).initiateScan();
+    }
+
+
     /**
      *
+     * @param requestCode
+     * @param resultCode
+     * @param intent
      */
-    public void llamar() {
-        try {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            SharedPreferences prefs = getSharedPreferences("preferencias",MODE_PRIVATE);
-            String tlf_motor = prefs.getString("tlf_motor","Ajuste valores");
-            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + tlf_motor)));
-        }catch(Exception e){
-            e.printStackTrace();
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        switch (requestCode) {
+            case IntentIntegrator.REQUEST_CODE:
+                if (resultCode == Activity.RESULT_OK) {
+
+                    IntentResult intentResult =
+                            IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+                    if (intentResult != null) {
+
+                        String contents = intentResult.getContents();
+                        String format = intentResult.getFormatName();
+
+                        //this.elemQuery.setText(contents);
+                        //this.resume = false;
+                        Log.d("SEARCH_EAN", "OK, EAN: " + contents + ", FORMAT: " + format);
+                    } else {
+                        Log.e("SEARCH_EAN", "IntentResult je NULL!");
+                    }
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    Log.e("SEARCH_EAN", "CANCEL");
+                }
         }
     }
+
+
 }
