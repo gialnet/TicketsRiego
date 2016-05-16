95491f85ac211435f17a40bb7098689e64f83ace
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2016-05-16 11:48:56 +0200

    Ajustes y arrancar motor

--- app/src/main/java/es/prodacon/movil/ticketsriego/ScrollingActivity.java
+++ app/src/main/java/es/prodacon/movil/ticketsriego/ScrollingActivity.java
@@ -1,20 +1,30 @@
 package es.prodacon.movil.ticketsriego;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import static es.prodacon.movil.ticketsriego.R.xml.preferencias;
+
 public class ScrollingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pedir_recibos);
+        setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,6 +36,31 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    /**
+     * Arrancar el motor
+     * @param view
+     */
+    public void execArrancaMotor(View view) {
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+                return;
+            }
+            SharedPreferences prefs = getSharedPreferences("preferencias",MODE_PRIVATE);
+            String tlf_motor = prefs.getString("tlf_motor","Ajuste valores");
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + tlf_motor)));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -44,8 +79,33 @@ public class ScrollingActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, PreferenciasActivity.class));
             return true;
         }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     *
+     */
+    public void llamar() {
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            SharedPreferences prefs = getSharedPreferences("preferencias",MODE_PRIVATE);
+            String tlf_motor = prefs.getString("tlf_motor","Ajuste valores");
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + tlf_motor)));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
