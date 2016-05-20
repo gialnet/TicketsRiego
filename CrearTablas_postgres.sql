ec4bd8791539d1549e1f7f05457a7a6972e10dba
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2016-05-20 13:34:15 +0200

    Se modifica la tabla de tickets

--- CrearTablas_postgres.sql
+++ CrearTablas_postgres.sql
@@ -3,12 +3,15 @@
 -- Tickets de riego
 --
 CREATE TABLE Tickets (
-    id              serial not null,
-    nticket         integer,
-    canal_compra    varchar(20) default 'papel',
-    fecha           timestamp default now(),
-    GeoPos          point,
-    observaciones   text,
+    id                  serial not null,
+    estanque            integer,
+    nticket             integer,
+    canal_compra        varchar(20) default 'papel',
+    minutos_comprados   integer,
+    minutos_servidos    integer,
+    fecha               timestamp default now(),
+    GeoPos              point,
+    observaciones       text,
     primary key(id)
 );
 create index Tickets_nticket on Tickets(nticket);
